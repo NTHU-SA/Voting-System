69ffbdb1b3c985871fed3b0e14b1d6139459c8a2
Wei <kiwi072400.lin@gmail.com>
2025-11-20 23:15:40 +0800

    Remove useActivityStatus hook and exports
    Deleted the useActivityStatus.tsx file and removed related exports from hooks/index.ts. This cleans up unused UI/status hooks and their utilities.

--- hooks/index.ts
+++ hooks/index.ts
@@ -7,6 +7,3 @@ export { useActivities } from "./useActivities";
 export { useActivity } from "./useActivity";
 export { useAdminActivities } from "./useAdminActivities";
 export { useAdminActivity } from "./useAdminActivity";
-
-// UI/Status hooks
-export { useActivityStatusBadge, getActivityStatus } from "./useActivityStatus";
